d2e0cff3c68307bec2aa8b7b17eef2fb691a7bbf
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2017-04-05 16:41:53 -0400

    updated the contact form

--- Views/content/contact.php
+++ Views/content/contact.php
@@ -3,6 +3,25 @@
         <div class="col-md-offset-3 col-md-6">
             <h1>Contact Us</h1>
 
+            <form method="post" action="">
+                <div class="form-group">
+                    <label for="FullNameTextBox">Full Name:</label>
+                    <input type="text" class="form-control" id="FullNameTextBox" name="fullname" placeholder="Enter Full Name" required>
+                </div>
+                <div class="form-group">
+                    <label for="EmailTextBox">Email:</label>
+                    <input type="email" class="form-control" id="EmailTextBox" name="email" placeholder="Enter Your Email" required>
+                </div>
+                <div class="form-group">
+                    <label for="ContactNumberTextBox">Contact Number:</label>
+                    <input type="tel" class="form-control" id="ContactNumberTextBox" name="contactnumber" placeholder="Enter Your Contact Number" required>
+                </div>
+                <div class="form-group">
+                    <label for="MessageTextArea">Message:</label>
+                    <textarea class="form-control" rows="5" id="MessageTextArea" name="contactnumber" placeholder="Enter Your Contact Number" required></textarea>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
 
 
         </div>
