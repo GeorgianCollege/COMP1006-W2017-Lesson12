53655f87b89c1af582a7d82236b5ce6adf60075d
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2017-04-05 15:35:49 -0400

    changed db config

--- Config/database.php
+++ Config/database.php
@@ -5,19 +5,21 @@ if(session_status() == PHP_SESSION_NONE) {
     session_start();
 }
 
-// cleardb access
-//$dsn = 'mysql:host=ca-cdbr-azure-central-a.cloudapp.net;dbname=videogamesdb';
-//$Username = 'b6ee96bd470785';
-//$Password = 'dc381279';
 
 function DBConnection() {
     // exception handling - use a try / catch
     try {
 
+        // cleardb access
+        $dsn = 'mysql:host=ca-cdbr-azure-central-a.cloudapp.net;dbname=videogamesdb';
+        $Username = 'b6ee96bd470785';
+        $Password = 'dc381279';
+
+
         //local db access
-        $dsn = 'mysql:host=localhost;dbname=gamedb';
-        $Username = 'dasha';
-        $Password = '12345';
+        //$dsn = 'mysql:host=localhost;dbname=gamedb';
+        //$Username = 'dasha';
+        //$Password = '12345';
         // instantiates a new pdo - an db object
         return new PDO($dsn, $Username, $Password);
     }
